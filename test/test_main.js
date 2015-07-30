7a33ef3b2f35e978cd18766651aea3d11763302b
Scisco <scisco7@gmail.com>
2015-07-30 14:29:33 -0400

    added mongo tests

--- test/test_main.js
+++ test/test_main.js
@@ -1,4 +1,4 @@
-/* global describe, it, after */
+/* global describe, it, after, before */
 'use strict';
 
 var fs = require('fs-extra');
@@ -6,8 +6,10 @@ var expect = require('chai').expect;
 var nock = require('nock');
 var Updater = require('../index.js');
 var client = require('../libs/connections.js').elasticsearch;
+var MongoDb = require('../libs/connections.js').mongodb;
+var Landsat = require('../libs/mongo/model.js');
 
-describe('Test the package', function () {
+describe('Test the package with elasticsearch', function () {
   this.timeout(1000000);
   var testIndex = 'test_main';
   var testType = '8';
@@ -64,6 +66,74 @@ describe('Test the package', function () {
     }).catch(function (err) {
       console.log(err);
       expect(err).to.be.undefined;
+      done();
+    });
+  });
+});
+
+describe('Test the package with mongoDB', function () {
+  this.timeout(1000000);
+  var testIndex = 'test_main';
+  var testType = '8';
+  var downloadDir = __dirname + '/download';
+  var db;
+  var dbUrl;
+
+  before(function (done) {
+    // Connect to MongoDb
+    db = new MongoDb(process.env.DBNAME || 'landsat-api', dbUrl);
+    db.start(done);
+  });
+
+  it('add records', function (done) {
+    var csv = fs.readFileSync(__dirname + '/test.csv', {encoding: 'utf8'});
+
+    nock('http://landsat.usgs.gov')
+      .get('/metadata_service/bulk_metadata_files/LANDSAT_8.csv')
+      .reply(200, csv);
+
+    var u = new Updater(testIndex, testType, 5, downloadDir);
+
+    // Update Elastic Search
+    u.updateMongoDb(dbUrl, function (err, msg) {
+      if (err) {
+        console.log('Error:', err);
+      } else {
+        expect(msg).to.equal('\nProcess is complete!');
+        done();
+      }
+    });
+  });
+
+  it('test record count', function (done) {
+    Landsat.count({}, function (err, count) {
+      // console.log(err);
+      expect(err).to.be.null;
+      expect(count).to.equal(14);
+      done();
+    });
+  });
+
+  it('check a record', function (done) {
+    Landsat.find({sceneID: 'LC81630762015162LGN00'}, function (err, record) {
+      expect(err).to.be.null;
+      expect(record[0].boundingBox.coordinates[0]).to.have.length.within(4, 6);
+      expect(record[0].row).to.equal(76);
+      done();
+    });
+  });
+
+  it('check another record', function (done) {
+    Landsat.find({sceneID: 'LC81630752015162LGN00'}, function (err, record) {
+      expect(err).to.be.null;
+      expect(record[0].boundingBox.coordinates[0]).to.have.length.within(4, 6);
+      expect(record[0].row).to.equal(75);
+      done();
+    });
+  });
+
+  after(function (done) {
+    db.deleteDb(function () {
       done();
     });
   });
