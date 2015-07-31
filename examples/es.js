771c5eb9379edeaf73ad19f6e91c0490a478cf19
Scisco <scisco7@gmail.com>
2015-07-31 12:07:40 -0400

    simplify example

--- examples/es.js
+++ examples/es.js
@@ -1,20 +1,11 @@
 'use strict';
 
 var Updater = require('../index.js');
-var async = require('async');
 
 var u = new Updater('landsat', '8', 1000);
 
-async.waterfall([
-
-  // Update Elastic Search
-  function (callback) {
-    u.updateEs(callback);
-  }
-], function (err, msg) {
-  if (err) {
-    console.log('Error:', err);
-  }
+u.updateEs(function (err, msg) {
+  console.log('Error:', err);
   console.log(msg);
   process.exit();
 });
