6e91de5d785e729315b528d0dc1136cfb06d47d0
Scisco <scisco7@gmail.com>
2015-08-06 14:48:44 -0400

    improve example

--- examples/mongo.js
+++ examples/mongo.js
@@ -6,7 +6,7 @@ var async = require('async');
 
 var u = new Updater('landsat', '8', 1000);
 
-var dbUrl = 'mongodb://localhost/landsat-new';
+var dbUrl = 'mongodb://localhost/landsat-api' || process.env.DBURL;
 
 async.waterfall([
   // Connect to MongoDb
